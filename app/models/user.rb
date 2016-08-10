5c6d8033b92bfbf939b4e84e1f3c6483714c039d
Micah Redding <micahtredding@gmail.com>
2016-08-10 12:55:38 -0400

    Adding Team Member join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :messages, class_name: 'Message', foreign_key: 'creator_id'
   has_many :created_teams, class_name: 'Team', foreign_key: 'creator_id'
+  has_many :belonged_to_teams, class_name: 'Team', through: 'team_member'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810162901) do
+ActiveRecord::Schema.define(version: 20160810164530) do
 
   create_table "channels", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160810162901) do
     t.datetime "updated_at", null: false
     t.integer  "channel_id"
     t.index ["creator_id"], name: "index_messages_on_creator_id"
+  end
+
+  create_table "team_members", force: :cascade do |t|
+    t.integer  "team_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_team_members_on_team_id"
+    t.index ["user_id"], name: "index_team_members_on_user_id"
   end
 
   create_table "teams", force: :cascade do |t|
