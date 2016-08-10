7e4552044d3dd3a68ac0ed460cbb7742a40db915
Micah Redding <micahtredding@gmail.com>
2016-08-10 11:43:26 -0400

    Adding Messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :messages, class_name: 'Message', foreign_key: 'creator_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810140332) do
+ActiveRecord::Schema.define(version: 20160810145134) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "creator_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_messages_on_creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
