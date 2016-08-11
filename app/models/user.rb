1430661768473561c14b3be6208a9481a127aa64
Micah Redding <micahtredding@gmail.com>
2016-08-10 22:51:36 -0400

    Fixing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :messages, class_name: 'Message', foreign_key: 'creator_id'
-  has_many :read_messages, class_name: 'Message', through: 'message_readers'
   has_many :created_teams, class_name: 'Team', foreign_key: 'creator_id'
-  has_many :teams, class_name: 'Team', through: 'team_members'
   has_many :created_channels, class_name: 'Channel', foreign_key: 'creator_id'
+
+  has_many :message_readers
+  has_many :team_members
+  has_many :channel_members
+
+  has_many :read_messages, class_name: 'Message', through: 'message_readers', source: 'message'
+  has_many :teams, class_name: 'Team', through: 'team_members'
   has_many :channels, class_name: 'Channel', through: 'channel_members'
 end
