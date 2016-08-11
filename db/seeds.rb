1827933f74bd4597dde758d355740b1e97fe7822
Micah Redding <micahtredding@gmail.com>
2016-08-11 11:11:50 -0400

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.create email: 'user@example.com', password: 'password'
+
+t1 = Team.create name: 'team1', visibility: 'public',  creator: u
+t2 = Team.create name: 'team2', visibility: 'public',  creator: u
+t3 = Team.create name: 'team1', visibility: 'private', creator: u
+
+c1 = Channel.create name: 'channel1', visibility: 'public',  team: t1, creator: u
+c2 = Channel.create name: 'channel2', visibility: 'public',  team: t1, creator: u
+c3 = Channel.create name: 'channel3', visibility: 'private', team: t1, creator: u
+
+m1 = Message.create content: 'message1', channel: c1, creator: u
+m2 = Message.create content: 'message2', channel: c1, creator: u
+m3 = Message.create content: 'message3', channel: c1, creator: u
