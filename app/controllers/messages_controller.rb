01030f86065c206be210216233a03d4f37839423
Micah Redding <micahtredding@gmail.com>
2016-08-11 12:07:19 -0400

    Adding messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+  def index
+    @team = Team.find(params[:team_id])
+    @channel = Channel.find(params[:channel_id])
+    @messages = @channel.messages
+  end
+
+  def new
+    @team = Team.find(params[:team_id])
+    @channel = Channel.find(params[:channel_id])
+    @message = Message.new
+  end
+
+  def create
+    @team = Team.find(params[:team_id])
+    @channel = Channel.find(params[:channel_id])
+    message_params = params.require(:message).permit(:content)
+    @message = Message.new(message_params)
+    @message.channel = @channel
+    @message.creator = current_user
+    if @message.save
+      redirect_to team_channel_messages_path(@team, @channel), notice: 'Yes'
+    else
+      redirect_to new_team_channel_message_path(@team, @channel), notice: 'No'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :teams do
-    resources :channels
+    resources :channels do
+      resources :messages, only: [:index, :new, :create]
+    end
   end
   root 'welcome#home'
 end
