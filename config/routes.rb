edcd8a6c5b5056f2d4057511f4ec6b7d01dc135f
Micah Redding <micahtredding@gmail.com>
2016-08-12 08:17:12 -0400

    Adding channel resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :teams
+  resources :teams do
+    resources :channels
+  end
 
   root 'welcome#home'
 end
